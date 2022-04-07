6ca24383f0d9723f98eeb3de90c2ec145503f591
mawalu <web@mawalabs.de>
2022-04-07 11:32:09 +0200

    Flakeify

---
+++ bwbox.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "1.0.0"
+author        = "mawalu"
+description   = "A DNS server for the ACME DNS-01 challenge"
+license       = "MIT"
+srcDir        = "."
+bin           = @["bwbox"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.0"
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "An experimental sandbox tool for linux apps";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      nimPackages.buildNimPackage {
+        name = "bwbox";
+        src = self;
+      };
+
+  };
+}
